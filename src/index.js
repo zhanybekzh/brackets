e10072836242066fbdbcc3bd2e0c3eb9bd91e07b
Zhandos <zh.zhanybek@gmail.com>
2024-10-07 22:36:49 +0500

    write function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+    let bracketMap = {};
+    let openBrackets = [];
+    let sameBrackets = new Set();
+  
+    bracketsConfig.forEach(([open, close]) => {
+      bracketMap[close] = open;
+      openBrackets.push(open);
+      if (open === close) {
+        sameBrackets.add(open);
+      }
+    });
+  
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+  
+      if (openBrackets.includes(char)) {
+        if (sameBrackets.has(char) && stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      } else {
+        let lastOpenBracket = stack.pop();
+        if (bracketMap[char] !== lastOpenBracket) {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0;
+  }
+  
